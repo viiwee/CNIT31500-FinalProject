e92536902de4506a4ae6fa89945709bc677a5808
viiwee <viiwee@live.com>
2021-04-25 20:00:54 -0400

    First

---
+++ cmake-build-debug-cygwin/CMakeFiles/CNIT31500-FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CNIT31500-FinalProject.dir/main.c.o"
+  "CNIT31500-FinalProject.pdb"
+  "CNIT31500-FinalProject.exe"
+  "libCNIT31500-FinalProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CNIT31500-FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
